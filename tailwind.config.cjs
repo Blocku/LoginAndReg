e2ad216961c42709a6ad18ddf61c00e895dee2a3
Blocku b <urent111@yandex.ru>
2024-04-03 15:28:36 +0000

    2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,16 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    fontFamily: {
+      display: ['Source Serif Pro', 'Georgia', 'serif'],
+      body: ['Synonym', 'system-ui', 'sans-serif'],
+    },
     extend: {
       colors: {
         
       }
-    },
+
+    }
   },
 
   plugins: [],
