119d580234ec6866e63f72eaf3bad95cf8fa9f8b
Blocku b <urent111@yandex.ru>
2024-04-09 11:40:39 +0000

    1

---
+++ src/components/BtnMenu.svelte
@@ -0,0 +1,27 @@
+<button  class="md:hidden border-2 border-zinc-100 p-1 rounded-lg" >
+    <svg class="w-7 h-6 stroke-[1.5px] stroke-black fill-none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>          
+</button>
+<div class:open={isMenuOpen} class="w-32 h-32 absolute border border-black right-3 top-14" >
+
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </nav>
+
+</div>
+
+
+<script lang='ts' >
+
+    let isMenuOpen = false;
+
+</script>
+
+
+<!-- "M6 18 18 6M6 6l12 12" -->
